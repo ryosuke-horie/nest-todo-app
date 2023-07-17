9771aec29afcc09be5ff244377a97f00591ed3cc
ryosuke-horie <ryosuke03140@gmial.com>
2023-07-17 21:16:19 +0900

    コントローラーでサービスを利用し、モジュールでリポジトリをimport

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,45 +2,49 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TaskPropertyDto } from './dto/task-property.dto';
 import { TaskStatusPipe } from './pipe/task-status.pipe';
+import { Task } from './task.entity';
+import { TasksService } from './tasks.service';
 
 // taskコントローラー（ルーティング）
 @Controller('tasks')
 export class TasksController {
+    constructor(
+        private tasksService: TasksService
+    ) {}
+
     // 登録されているタスク取得
     @Get()
-    getTasks() {
-        return "getTasks Success!"
+    getTasks(): Promise<Task[]> {
+        return this.tasksService.getTasks();
     }
 
     // [id]のタスク取得
     @Get('/:id')
     getTaskById(
-        @Param('id', ParseIntPipe) id: number) {
-        return `getTaskById Success! Parameter [id:${id}]`
+        @Param('id', ParseIntPipe) id: number): Promise<Task> {
+        return this.tasksService.getTaskById(id);
     }
 
     // 新規タスク登録
     @Post()
-    @UsePipes(ValidationPipe) // @UsePipesを付与したメソッドはバリデーションパイプ(今回でいうとTaskPropertyDtoで定義した値の空検証)が有効になります
+    @UsePipes(ValidationPipe)
     createTask(
-        @Body() taskPropertyDto: TaskPropertyDto) {
-        const { title, description } = taskPropertyDto
-        return `createTask Success! Prameter [title:${title}, descritpion:${description}]`
+        @Body() taskPropertyDto: TaskPropertyDto
+        ): Promise<Task> {
+        return this.tasksService.createTask(taskPropertyDto);
     }
 
     // 登録されている[id]のタスク削除
     @Delete('/:id')
     deleteTask(
-        @Param('id', ParseIntPipe) id: number) {
-        return `deleteTask Success! Prameter [id:${id}]`
+        @Param('id', ParseIntPipe) id: number): Promise<void> {
+        return this.tasksService.deleteTask(id);
     }
 
-    // 登録されている[id]のタスク更新
     @Patch('/:id')
     updateTask(
-        // パイプを使用して、idを数値型に変換
         @Param('id', ParseIntPipe) id: number,
-        @Body('status') status: string) {
-        return `updateTask Success! Prameter [id:${id}, status:${status}]`
+        @Body('status',TaskStatusPipe) status: string ): Promise<Task> {
+        return this.tasksService.updateTask(id, status);
     }
 }
