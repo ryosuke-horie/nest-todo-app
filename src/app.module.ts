1b24849c8795278820e8c5e968567da9d2eb7495
ryosuke-horie <ryosuke03140@gmial.com>
2023-07-17 20:31:32 +0900

    ORMでPostgresに接続

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    // importsに追加
+    TypeOrmModule.forRoot({
+      type: 'postgres',       // DBの種類
+      port: 5432,             // 使用ポート
+      database: 'postgres',    // データベース名
+      host: 'localhost',      // DBホスト名
+      username: 'postgres',       // DBユーザ名
+      password: 'passw0rd',       // DBパスワード
+      synchronize: true,      // モデル同期(trueで同期)
+      entities: [__dirname + '/**/*.entity.{js,ts}'],  // ロードするエンティティ
+    })
+  ],
 })
 export class AppModule {}
