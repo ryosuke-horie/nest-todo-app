784de54cb4a3b6c74e23da701f4b45ca4926729a
ryosuke-horie <ryosuke03140@gmial.com>
2023-07-17 20:35:21 +0900

    Entity作成

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,16 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Task extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: string;
+}
